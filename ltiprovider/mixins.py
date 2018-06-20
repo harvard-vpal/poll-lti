0aed21e6bbe344dd5f9296dfb9864427e258a8b3
Andrew Ang <ang.kunanit@gmail.com>
2018-06-19 22:47:46 -0400

    Refactor mixins to better utilize lti tool_provider class

--- ltiprovider/mixins.py
+++ ltiprovider/mixins.py
@@ -8,8 +8,8 @@ from lti import InvalidLTIRequestError
 from lti.contrib.django import DjangoToolProvider
 from oauthlib import oauth1
 
-from apps.ltiprovider.validator import SignatureValidator
-from apps.ltiprovider.models import LtiUser, LtiConsumer
+from ltiprovider.validator import SignatureValidator
+from ltiprovider.models import LtiUser, LtiConsumer
 
 
 log = logging.getLogger(__name__)
@@ -28,29 +28,58 @@ class LtiLaunchMixin(object):
     @csrf_exempt
     @xframe_options_exempt
     def dispatch(self, request, *args, **kwargs):
-        validate_lti_request(request)
-        get_or_create_lti_user(request)
-        request.session['lti_session'] = request.POST['oauth_nonce']
-        # put lti user id in session
-        request.session['lti_user_id'] = request.POST['user_id']
+        tool_provider = DjangoToolProvider.from_django_request(request=request)
+        validate_lti_request(tool_provider)
+        get_or_create_lti_user(tool_provider)
+        store_lti_params_in_session(request.session, tool_provider)
+        add_lti_session_flag(request.session)
         return super(LtiLaunchMixin, self).dispatch(request, *args, **kwargs)
 
 
 class LtiSessionMixin(object):
     """
     Mixin that enforces that user has an LTI session
-    LTI session is indicated by the existence of the 'lti_session' session key/value
+    LTI session is indicated by the existence of the 'LTI_SESSION' session key/value
     """
     @xframe_options_exempt
     def dispatch(self, request, *args, **kwargs):
-        lti_session = request.session.get('lti_session')
+        lti_session = request.session.get('LTI_SESSION')
         if not lti_session:
             log.error('LTI session is not found, Request cannot be processed')
             raise PermissionDenied("Content is available only through LTI protocol.")
         return super(LtiSessionMixin, self).dispatch(request, *args, **kwargs)
 
 
-def validate_lti_request(request):
+def add_lti_session_flag(session):
+    """
+    Adds LTI_SESSION value to session to denote an LTI session
+    :param session:
+    :return:
+    """
+    session['LTI_SESSION'] = True
+
+
+def check_if_lti_session(request):
+    """
+    Checks if request is part of a lti user session
+    :param request: django request object
+    :return:
+    """
+    return request.session.get('LTI_SESSION', False)
+
+
+def store_lti_params_in_session(session, tool_provider):
+    """
+    Store LTI launch params in request session
+    :param session: django request.session
+    :param tool_provider: lti tool_provider object to get params from
+    :return:
+    """
+    for prop, value in tool_provider.to_params().items():
+        session[prop] = value
+
+
+def validate_lti_request(tool_provider):
     """
     Check if LTI launch request is valid, and raise an exception if request is not valid
     An LTI launch is valid if:
@@ -59,11 +88,9 @@ def validate_lti_request(request):
     :param request:
     :return: none
     """
-    tool_provider = DjangoToolProvider.from_django_request(request=request)
-
     # if using reverse proxy, validate based on originating protocol
     # by replacing http with https in the url used for validation
-    if request.META.get('HTTP_X_FORWARDED_PROTO') == 'https':
+    if tool_provider.launch_headers.get('HTTP_X_FORWARDED_PROTO') == 'https':
         tool_provider.launch_url = tool_provider.launch_url.replace('http:', 'https:', 1)
 
     validator = SignatureValidator()
@@ -76,7 +103,7 @@ def validate_lti_request(request):
         raise Http404('LTI request is not valid')
 
 
-def get_or_create_lti_user(request):
+def get_or_create_lti_user(tool_provider):
     """
     Get or create lti user based on lti launch params:
         'user_id',
@@ -85,21 +112,18 @@ def get_or_create_lti_user(request):
     Handle some cases where these request parameters are not found or invalid
     :return: (LtiUser model instance, bool)
     """
-    # user id
-    user_id = request.POST.get('user_id')
-    if not user_id:
-        raise Exception('User id not in lti launch parameters')
+    user_id = tool_provider.launch_params.get('user_id')
 
-    # consumer key
-    lti_consumer = LtiConsumer.objects.get(consumer_key=request.POST.get('oauth_consumer_key'))
+    # get lti consumer model instance
+    lti_consumer = LtiConsumer.objects.get(consumer_key=tool_provider.launch_params.get('oauth_consumer_key'))
 
-    # tool consumer instance guid
-    tool_consumer_instance_guid = request.POST.get('tool_consumer_instance_guid')
+    # tool consumer instance guid - set using default for lti consuumer if missing
+    tool_consumer_instance_guid = tool_provider.launch_params.get('tool_consumer_instance_guid')
     if not tool_consumer_instance_guid:
-        # tool_consumer_instance_guid = infer_tool_consumer_instance_guid(request)
         tool_consumer_instance_guid = lti_consumer.default_tool_consumer_instance_guid
+        # TODO possibly infer a tool_consumer_instance_guid value based on request origin
 
-    # get or create the lti user
+    # get or create the lti user model instance
     lti_user, created = LtiUser.objects.get_or_create(
         user_id=user_id,
         lti_consumer=lti_consumer,
